bdda2fb155a7e66897f9787b90439f7e125ef211
Olivier Lischer <olivier.lische@liolin.ch>
2018-08-06 21:38:49 +0200

    Add sorting by switching

--- js/pjsm.js
+++ js/pjsm.js
@@ -2,28 +2,45 @@
  * A pure JavaScript multiselect library.
  *
  * @author Olivier Lischer <olivier.lischer@liolin.ch>
- * @version 0.1.0
+ * @version 0.2.0
  */
 
 /** Item Switch callback **/
 const switchItem = function(obj, multiselect){
+    let ul = [];
+    let selected = false;
     obj.parentElement.removeChild(obj);
 
     if(obj.className === 'pjsm-enabled-item'){
 	obj.className = 'pjsm-available-item';
-	multiselect.container.querySelector('.pjsm-available .pjsm-list').appendChild(obj);
-	multiselect.object.querySelector('.'+obj.querySelector('span').className.replace('span', 'option')).selected = false;
+	ul = multiselect.available.querySelector('.pjsm-list');
+	selected = false;
     }
     else{
-	obj.className = 'pjsm-enabled-item';	
-	multiselect.container.querySelector('.pjsm-enabled .pjsm-list').appendChild(obj);
-	multiselect.object.querySelector('.'+obj.querySelector('span').className.replace('span', 'option')).selected = true;
+	obj.className = 'pjsm-enabled-item';
+	ul = multiselect.enabled.querySelector('.pjsm-list');
+	selected = true;
     }
+
+    let li = ul.getElementsByTagName('li');
+    let inserted = false;
+
+    for(let i = 0; i < li.length; i++){
+	if(obj.childNodes[0].innerText < li[i].childNodes[0].innerText){
+	    ul.insertBefore(obj, li[i]);
+	    inserted = true;
+	    break;
+	}
+    }
+    if(!inserted){
+	ul.insertBefore(obj, null);
+    }
+    multiselect.object.querySelector('.'+obj.querySelector('span').className.replace('span', 'option')).selected = selected;
 };
 
 function Multiselect(object){
-    /** functions **/ 
-    this.createElement = function(tag, options){
+    /** functions **/
+    this.__createElement = function(tag, options){
 	let object = document.createElement(tag);
 	for(let key in options){
 	    let attribute = document.createAttribute(key);
@@ -32,27 +49,26 @@ function Multiselect(object){
 	}
 	return object;
     }
-    this.insertAfter = function(newNode, after){
+    this.__insertAfter = function(newNode, after){
 	after.parentNode.insertBefore(newNode, after);
     }
-
     
     /** class variabels **/
     this.object = object;
     this.object.style.display = 'none';
-    this.container = this.createElement('div', {'class': 'pjsm-container'});
-    this.available = this.createElement('div', {'class': 'pjsm-available'});
-    this.middle = this.createElement('div', {'class': 'pjsm-middle'});
-    this.enabled = this.createElement('div', {'class': 'pjsm-enabled'});
-    this.list = this.createElement('ul', {'class': 'pjsm-list'});
-    this.emptyList = this.createElement('ul', {'class': 'pjsm-list'});
+    this.container = this.__createElement('div', {'class': 'pjsm-container'});
+    this.available = this.__createElement('div', {'class': 'pjsm-available'});
+    this.middle = this.__createElement('div', {'class': 'pjsm-middle'});
+    this.enabled = this.__createElement('div', {'class': 'pjsm-enabled'});
+    this.list = this.__createElement('ul', {'class': 'pjsm-list'});
+    this.emptyList = this.__createElement('ul', {'class': 'pjsm-list'});
     const that = this;
 
 
     /** get values from select field **/
     for(let i = 0; i < this.object.options.length; i++){
-	let li = this.createElement('li');
-	let span = this.createElement('span');
+	let li = this.__createElement('li');
+	let span = this.__createElement('span');
 
 	this.object[i].className = 'pjsm-'+i+'-option';
 	span.innerText = this.object[i].text;
@@ -68,7 +84,7 @@ function Multiselect(object){
 
     
     /** init multiselect html **/
-    this.insertAfter(this.container, this.object);
+    this.__insertAfter(this.container, this.object);
     this.available.appendChild(this.list);
     this.container.appendChild(this.available);
     this.container.appendChild(this.middle);
